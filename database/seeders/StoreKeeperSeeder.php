0641c4719e69fd3ebd7d3ce48e4307feac1784b3
kaksity <daudapona2@gmail.com>
2022-07-25 10:53:53 +0100

    Bug Fix

---
+++ database/seeders/StoreKeeperSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+// use App\Models\Admin;
+use App\Models\{User,Role,Profile, SuperAdmin as ModelsSuperAdmin};
+
+class StoreKeeperSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    }
+}
